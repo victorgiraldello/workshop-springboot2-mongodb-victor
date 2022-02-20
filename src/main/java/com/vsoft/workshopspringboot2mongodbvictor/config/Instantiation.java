778d8291fc843de2e3fbb77802625146cf6c913d
Victor Giraldello <victor.giraldello@gmail.com>
2022-02-20 18:21:18 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/com/vsoft/workshopspringboot2mongodbvictor/config/Instantiation.java
@@ -0,0 +1,27 @@
+package com.vsoft.workshopspringboot2mongodbvictor.config;
+
+import com.vsoft.workshopspringboot2mongodbvictor.domain.User;
+import com.vsoft.workshopspringboot2mongodbvictor.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
