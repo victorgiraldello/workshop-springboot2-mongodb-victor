6f2ebb495a253f1f6fa65c3bf3430c77bc9ad36d
Victor Giraldello <victor.giraldello@gmail.com>
2022-02-20 17:05:05 -0300

    Primeiro commit - Projeto criado

---
+++ src/main/java/com/vsoft/workshopspringboot2mongodbvictor/WorkshopSpringboot2MongodbVictorApplication.java
@@ -0,0 +1,13 @@
+package com.vsoft.workshopspringboot2mongodbvictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringboot2MongodbVictorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopSpringboot2MongodbVictorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vsoft/workshopspringboot2mongodbvictor/WorkshopSpringboot2MongodbVictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsoft.workshopspringboot2mongodbvictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringboot2MongodbVictorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
