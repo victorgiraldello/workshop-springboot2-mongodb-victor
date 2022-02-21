5a845d93c96c9f2e4491e0de77795f651d533a22
Victor Giraldello <victor.giraldello@gmail.com>
2022-02-20 22:05:48 -0300

    Consulta simples com query methods

--- src/main/java/com/vsoft/workshopspringboot2mongodbvictor/services/PostService.java
+++ src/main/java/com/vsoft/workshopspringboot2mongodbvictor/services/PostService.java
@@ -6,6 +6,7 @@ import com.vsoft.workshopspringboot2mongodbvictor.services.exception.ObjectNotFo
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,6 +18,10 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return repo.findByTitleContainingIgnoreCase(text);
     }
 
 }
