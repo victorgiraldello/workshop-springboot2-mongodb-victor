2a245db8f478d81dc30fff25ab7239ffe83ba828
Victor Giraldello <victor.giraldello@gmail.com>
2022-02-20 21:38:29 -0300

    Obtendo post por Id

---
+++ src/main/java/com/vsoft/workshopspringboot2mongodbvictor/services/PostService.java
@@ -0,0 +1,22 @@
+package com.vsoft.workshopspringboot2mongodbvictor.services;
+
+import com.vsoft.workshopspringboot2mongodbvictor.domain.Post;
+import com.vsoft.workshopspringboot2mongodbvictor.repository.PostRepository;
+import com.vsoft.workshopspringboot2mongodbvictor.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+}
